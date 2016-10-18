0b59ba778f25ad9d20ca93bfe25a584348366ab4
Eric Jung <me.jung@sta.samsung.com>
2016-10-17 20:41:36 -0700

    printed two digit of kernel level
    Change-Id: I64a14d42b02a2d70109f272ab0eba55df8ad8fe0

--- app/src/main/jni/src/core/dmesg.cc
+++ app/src/main/jni/src/core/dmesg.cc
@@ -4,8 +4,23 @@
  */
 
 #include "dmesg.h"
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
+#define KLOG_READ_ALL   3
+#define KLOG_LEN    (1 << 17)
 #include <android/log.h>
+#define LOG_TAG   "SOAK-jni"
+#define  ALOG(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
+#define BufferSize  256
+#define KLOG_SIZE_BUFFER  10
+#define KLOG_BUF_SHIFT    17      /* CONFIG_LOG_BUF_SHIFT from our kernel */
+#define KLOG_BUF_LEN      (1 << KLOG_BUF_SHIFT)
+#define SYS_BOOT_TIME "/proc/uptime"
 
 namespace com {
 namespace eolwral {
@@ -58,6 +73,7 @@ namespace core {
   {
     // finish the buffer
     auto mloc = CreatedmesgInfoList(*this->_flatbuffer, this->_flatbuffer->CreateVector(this->_list));
+
     FinishdmesgInfoListBuffer(*this->_flatbuffer, mloc);
   }
 
@@ -104,29 +120,40 @@ namespace core {
       offsetStart = offsetStart + procLineLen + 1;
 
       char message[BufferSize];
-      char level = '\x0';
+      //char level = '\x0';
+      char level[3] = "";
       unsigned long seconds = 0;
       int itemCounts= 0;
 
       memset(message, 0, BufferSize);
-
+      memset(level,0, sizeof(level));
       // detect log message format and parse it
+
       if(procLine[3] == '[')
       {
-        itemCounts = sscanf(procLine, "<%c>[%lu.%*06u] %[^\n]",
-                            &level,
-                            &seconds,
-                            message);
-        seconds += _bootTime+seconds;
+        itemCounts = sscanf(procLine, "<%1c>[%lu.%*06u] %[^\n]", &level[0], &seconds, message);
+        //itemCounts = sscanf(procLine, "<%c>[%lu.%*06u] %[^\n]", &level, &seconds, message);
+        //seconds += _bootTime+seconds;
+        ALOG("level = %c", (char)level[0]);
+        ALOG("seconds %ul", seconds);
+        ALOG("message %s", message);
+      }
+      else if (procLine[4] == '[')
+      {
+        itemCounts = sscanf(procLine, "<%c%c>[%lu.%*06u] %[^\n]", &level[0], &level[1], &seconds, message);
+        //seconds += _bootTime+seconds;
+        ALOG("level = %s", level);
+        ALOG("seconds %ul", seconds);
+        ALOG("message %s", message);
       }
       else
       {
         seconds = 0;
-        itemCounts = sscanf(procLine, "<%c>%[^\n]", &level, message);
+        itemCounts = sscanf(procLine, "<%c>%[^\n]", &level[0], message);
       }
 
       // push log into list
-      if (itemCounts == 2 || itemCounts == 3)
+      if (itemCounts == 2 || itemCounts == 3 || itemCounts == 4)
       {
         // save message
         auto msg = this->_flatbuffer->CreateString(message);
@@ -135,31 +162,38 @@ namespace core {
         dmesgInfoBuilder dmesgInfo(*this->_flatbuffer);
 
         dmesgInfo.add_seconds(seconds);
-
-        switch(level)
+        switch(strtol(level, NULL, 10))
         {
-        case '0':
+        //case '0':
+        case 0:
           dmesgInfo.add_level(dmesgLevel_EMERGENCY);
           break;
-        case '1':
+        //case '1':
+        case 1:
           dmesgInfo.add_level(dmesgLevel_ALERT);
           break;
-        case '2':
+        //case '2':
+        case 2:
           dmesgInfo.add_level(dmesgLevel_CRITICAL);
           break;
-        case '3':
+        //case '3':
+        case 3:
           dmesgInfo.add_level(dmesgLevel_ERROR);
           break;
-        case '4':
+        //case '4':
+        case 4:
           dmesgInfo.add_level(dmesgLevel_WARNING);
           break;
-        case '5':
+        //case '5':
+        case 5:
           dmesgInfo.add_level(dmesgLevel_NOTICE);
           break;
-        case '6':
+        //case '6':
+        case 6:
           dmesgInfo.add_level(dmesgLevel_INFORMATION);
           break;
-        case '7':
+        //case '7':
+        case 7:
           dmesgInfo.add_level(dmesgLevel_DEBUG);
           break;
         default:
